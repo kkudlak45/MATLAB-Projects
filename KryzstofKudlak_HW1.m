7a9e92f117fe2f782d05f9624441a8d295398982
kkudlak45 <46248021+kkudlak45@users.noreply.github.com>
2020-01-23 14:45:51 -0500

    Update KryzstofKudlak_HW1.m

--- KryzstofKudlak_HW1.m
+++ KryzstofKudlak_HW1.m
@@ -16,4 +16,33 @@ disp(['Answer to c is ', num2str(T)]);
 
 
 % Prob 2 
+a = 24.125487;
+b = 35;
+c = 22.316548;
+d = 19;
+
+fprintf('\nThe value of a is %f', a);
+fprintf('\nThe value of b is %i and c is %f', b, c);
+fprintf('\nC is equal to %f and B is equal to %i', c, b);
+fprintf('\nC is less than D\n');
+
+
+% Prob 3
+Q  = 8000;  % cal/mol
+R  = 1.987; % cal/(mol K)
+k0 = 1200;  % min^-1
+T  = 400;   % K
+
+k  = k0*exp(-Q/(R*T));
+fprintf('\nk = %.4f\n', k);
+
+
+% Prob 4
+P = 90000; % dollar
+y = 20;    % years
+r = 0.055;
+
+M = (P*r/12)/(1 - (1+r/12)^(-12*y));
+fprintf('\nM = $%.2f\n', M);
+
 
