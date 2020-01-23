90b767f75673ff1abb2639e76ca59035259621da
kkudlak45 <46248021+kkudlak45@users.noreply.github.com>
2020-01-23 14:15:14 -0500

    Add files via upload

---
+++ KryzstofKudlak_HW1.m
@@ -0,0 +1,19 @@
+% Kryzstof Kudlak
+% ENGR 101-H02
+% HW 1
+
+clc; clear;
+
+%Prob 1
+
+R = log10(24)/tand(10) + 4*exp(2) + 3*sqrt(120);
+S = (16 - 216/22)/(1.7^4 + 26) + 9.8 + 3*log(51);
+T = (6.4 + 5.2^3)/(1.3^2) + sin(4*pi);
+
+disp(['Answer to a is ', num2str(R)]);
+disp(['Answer to b is ', num2str(S)]);
+disp(['Answer to c is ', num2str(T)]);
+
+
+% Prob 2 
+
---
+++ Kudlak_Kryzstof_012320.m
@@ -0,0 +1,23 @@
+% Kryzstof Kudlak
+% ENGR 101-H02
+% ICA Vectors and Matrices
+
+clc; clear;
+
+% Vectors
+% Row Vectors
+a  = [3 7 9 11 5 10]   % 1   by 6
+aa = [3,7,9,11,5,10] % row by column
+
+% Column Vectors
+b  = [8; 4; 1; 7]      % 4 by 1
+bb = [9
+      4
+      3]
+  
+  
+% Shortcuts for creating vectors
+% Option 1
+c   = 2:2:10             % start:inc:toval
+cc  = 3:3:15  
+ccc = 5:10               % start:toval
