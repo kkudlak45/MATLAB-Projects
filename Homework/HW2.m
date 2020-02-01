eb6c1e059fa919e5558574ad3ab7d6af7a4abb88
kkudlak45 <46248021+kkudlak45@users.noreply.github.com>
2020-01-31 19:43:36 -0500

    HW2

---
+++ Homework/HW2.m
@@ -0,0 +1,19 @@
+% Kryzstof Kudlak
+% ENGR 102 -  H02
+% Problem 7 - HW 2
+
+clc; clear;
+
+tny = [31 26 30 29 33 33 39 41 41 34 33 45 42 36 39 37 45 43 36 41 37 32 35 42 38 33 40 37 36 51 50];
+tpitt = [37 24 28 25 21 28 46 37 36 20 24 31 34 40 43 36 34 41 42 35 38 36 35 33 42 42 37 26 20 25 31];
+
+fprintf('Average temp for NY: %.2f\n', mean(tny));
+fprintf('Average temp for Pitt: %.2f\n', mean(tpitt));
+fprintf('\nMax temp for NY: %.0f\n', max(tny));
+fprintf('Max temp for Pitt: %.0f\n', max(tpitt));
+fprintf('\nMin temp for NY: %.0f\n', min(tny));
+fprintf('Min temp for Pitt: %.0f\n', min(tpitt));
+
+
+
+
